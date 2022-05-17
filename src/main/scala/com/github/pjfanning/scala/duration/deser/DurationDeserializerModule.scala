a096c6e97d6b26c9f1e97b515ea3a868c362913c
PJ Fanning <pjfanning@users.noreply.github.com>
2022-05-17 10:45:53 +0100

    deser

--- src/main/scala/com/github/pjfanning/scala/duration/deser/DurationDeserializerModule.scala
+++ src/main/scala/com/github/pjfanning/scala/duration/deser/DurationDeserializerModule.scala
@@ -1,9 +1,10 @@
 package com.github.pjfanning.scala.duration.deser
 
 import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.deser.Deserializers
+import com.fasterxml.jackson.databind.deser.{Deserializers, KeyDeserializers}
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind._
+import com.fasterxml.jackson.databind.`type`.SimpleType
 import com.github.pjfanning.scala.duration.JacksonModule
 
 import java.time.{Duration => JavaDuration}
@@ -11,26 +12,43 @@ import scala.compat.java8.DurationConverters._
 import scala.concurrent.duration.FiniteDuration
 import scala.languageFeature.postfixOps
 
-private object FiniteDurationDeserializer extends StdDeserializer[FiniteDuration](classOf[FiniteDuration] ) {
-  private val JavaDurationClass = classOf[JavaDuration]
+private object FiniteDurationDeserializerShared {
+  val JavaDurationClass = classOf[JavaDuration]
+  lazy val JavaDurationType = SimpleType.constructUnsafe(JavaDurationClass)
+  val FiniteDurationClass = classOf[FiniteDuration]
+}
 
+private object FiniteDurationDeserializer extends StdDeserializer[FiniteDuration](classOf[FiniteDuration]) {
   override def deserialize(p: JsonParser, ctxt: DeserializationContext): FiniteDuration = {
-    Option(ctxt.readValue(p, JavaDurationClass)) match {
+    Option(ctxt.readValue(p, FiniteDurationDeserializerShared.JavaDurationClass)) match {
       case Some(duration) => duration.toScala
       case _ => None.orNull
     }
   }
 }
 
-private object FiniteDurationDeserializerResolver extends Deserializers.Base {
-  private val FiniteDurationClass = classOf[FiniteDuration]
+private object FiniteDurationKeyDeserializer extends KeyDeserializer {
+  override def deserializeKey(key: String, ctxt: DeserializationContext): AnyRef = {
+    val keyDeserializer = ctxt.findKeyDeserializer(FiniteDurationDeserializerShared.JavaDurationType, null)
+    keyDeserializer.deserializeKey(key, ctxt).asInstanceOf[JavaDuration].toScala
+  }
+}
 
+private object FiniteDurationDeserializerResolver extends Deserializers.Base {
   override def findBeanDeserializer(javaType: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): JsonDeserializer[FiniteDuration] =
-    if (FiniteDurationClass isAssignableFrom javaType.getRawClass)
+    if (FiniteDurationDeserializerShared.FiniteDurationClass isAssignableFrom javaType.getRawClass)
       FiniteDurationDeserializer
+    else None.orNull
+}
+
+private object FiniteDurationKeyDeserializerResolver extends KeyDeserializers {
+  override def findKeyDeserializer(javaType: JavaType, config: DeserializationConfig, beanDesc: BeanDescription): KeyDeserializer =
+    if (FiniteDurationDeserializerShared.FiniteDurationClass isAssignableFrom javaType.getRawClass)
+      FiniteDurationKeyDeserializer
     else None.orNull
 }
 
 trait DurationDeserializerModule extends JacksonModule {
   this += { _ addDeserializers FiniteDurationDeserializerResolver }
+  this += { _ addKeyDeserializers FiniteDurationKeyDeserializerResolver }
 }
